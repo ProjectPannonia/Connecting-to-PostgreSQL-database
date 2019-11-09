c0a66bd6c7d32ef6a70b9cabb9394b0f2d502f5f
Adam Letenyei <letenyeidam89@gmail.com>
2019-11-09 15:55:35 +0100

    Connection ready

--- src/Db.java
+++ src/Db.java
@@ -1,3 +1,21 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Db {
-    
+    private final String url = "jdbc:postgresql://localhost/LD";
+    private final String userName = "postgres";
+    private final String password = "Zetka67";
+
+    public Connection connect(){
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(url,userName,password);
+            System.out.println("Connected to the databese!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return connection;
+    }
 }
